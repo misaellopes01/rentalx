774651d9ab2bbb1f383fa260e073a98dfb3780dc
Misael Lopes <mecl.ely@gmail.com>
2021-12-25 04:06:55 +0100

    Chapter IV Completed

--- src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -8,7 +8,7 @@ import createConnection from "@shared/infra/typeorm";
 
 let connection: Connection;
 
-describe("Create Category Controller", () => {
+describe("List Category Controller", () => {
   beforeAll(async () => {
     connection = await createConnection();
     await connection.runMigrations();
@@ -23,18 +23,18 @@ describe("Create Category Controller", () => {
   });
 
   afterAll(async () => {
-    // await connection.dropDatabase();
+    await connection.dropDatabase();
     await connection.close();
   });
 
-  it("should be able to create a new category", async () => {
+  it("should be able to list all categories", async () => {
     const responseToken = await request(app).post("/sessions").send({
       email: "admin@rentx.com",
       password: "admin",
     });
     const { token } = responseToken.body;
 
-    const response = await request(app)
+    await request(app)
       .post("/categories")
       .send({
         name: "Category SuperTest",
@@ -44,26 +44,13 @@ describe("Create Category Controller", () => {
         Authorization: `Baerer ${token}`,
       });
 
-    expect(response.status).toBe(201);
-  });
+    const response = await request(app).get("/categories");
 
-  it("should not be able to create a new category with existing name", async () => {
-    const responseToken = await request(app).post("/sessions").send({
-      email: "admin@rentx.com",
-      password: "admin",
-    });
-    const { token } = responseToken.body;
+    console.log(response.body);
 
-    const response = await request(app)
-      .post("/categories")
-      .send({
-        name: "Category SuperTest",
-        description: "SuperTest",
-      })
-      .set({
-        Authorization: `Baerer ${token}`,
-      });
-
-    expect(response.status).toBe(400);
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(1);
+    expect(response.body[0]).toHaveProperty("id");
+    expect(response.body[0].name).toEqual("Category SuperTest");
   });
 });
